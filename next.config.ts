44e1d2bb4d0fece94c75be8cb0829fd1dc62cc76
bajrayejoon <binaya.bajrashakya@gmail.com>
2025-11-25 20:46:36 +0545

    feat: added husky and next configurations

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  poweredByHeader: false,
+  compress: true,
+  reactStrictMode: true,
+  productionBrowserSourceMaps: false,
+  typedRoutes: true,
+  images: {
+    unoptimized: false,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+    scrollRestoration: true,
+
+  }
 };
 
 export default nextConfig;
